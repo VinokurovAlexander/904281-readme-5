4d8a8534a2bae8d37f1f7f27ce6b5918c66ff5e2
Alexander Vinokurov <vinokurov.ag@mail.ru>
2024-11-10 17:11:32 +0300

    fix application start

--- project/apps/users/src/lib/mongo/getOptions.ts
+++ project/apps/users/src/lib/mongo/getOptions.ts
@@ -1,17 +1,21 @@
-import {MongooseModuleAsyncOptions} from "@nestjs/mongoose";
-import {ConfigService} from "@nestjs/config";
-import {getMongoConnectionString} from "@project/utils";
+import { MongooseModuleAsyncOptions } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { getMongoConnectionString } from '@project/utils';
 
 export const getMongooseOptions = (): MongooseModuleAsyncOptions => ({
-  useFactory: async (config: ConfigService) => ({
-    uri: getMongoConnectionString({
-      username: config.get<string>('db.user'),
-      password: config.get<string>('db.password'),
-      host: config.get<string>('db.host'),
-      port: config.get<number>('db.port'),
-      authDatabase: config.get<string>('db.authBase'),
-      databaseName: config.get<string>('db.name'),
-    })
-  }),
-  inject: [ConfigService]
-})
+    useFactory: async (config: ConfigService) => {
+        const mongoUri = getMongoConnectionString({
+            username: config.get<string>('db.user'),
+            password: config.get<string>('db.password'),
+            host: config.get<string>('db.host'),
+            port: config.get<number>('db.port'),
+            authDatabase: config.get<string>('db.authBase'),
+            databaseName: config.get<string>('db.name'),
+        });
+
+        return {
+            uri: mongoUri,
+        };
+    },
+    inject: [ConfigService],
+});
