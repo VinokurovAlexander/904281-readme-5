1b61fc5d94240614864d8bc3d36fa0328c23fe1e
Alexander Vinokurov <vinokurov.ag@mail.ru>
2025-02-05 10:44:06 +0300

    added http file

--- project/apps/posts/src/main.ts
+++ project/apps/posts/src/main.ts
@@ -8,15 +8,20 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+const VERSION = 'v1';
+const globalPrefix = `api/${VERSION}`;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix(globalPrefix);
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+
+    Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    );
 }
 
 bootstrap();
