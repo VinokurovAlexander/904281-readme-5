dd1eb9035f7345b02579793ceddf33e2a54d56a4
Alexander Vinokurov <vinokurov.ag@mail.ru>
2025-02-15 12:57:57 +0300

    added file-vault config

--- project/apps/file-vault/src/app/app.module.ts
+++ project/apps/file-vault/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { fileVaultConfig } from './config';
+import { ConfigModule } from '@nestjs/config';
+import { mongoConfig } from '../../../users/src/lib/mongo';
+
 @Module({
-    imports: [],
-    controllers: [AppController],
-    providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            cache: true,
+            load: [mongoConfig, fileVaultConfig],
+        }),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
