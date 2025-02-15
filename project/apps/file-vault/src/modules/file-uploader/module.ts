5af40fa730916b323e3926ed3702eb1a39bdd8bf
Alexander Vinokurov <vinokurov.ag@mail.ru>
2025-02-15 17:24:56 +0300

    added ServeStaticModule

--- project/apps/file-vault/src/modules/file-uploader/module.ts
+++ project/apps/file-vault/src/modules/file-uploader/module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
 import { mongoConfig } from '@project/config';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { fileVaultConfig } from './config';
 import { FileUploaderController } from './controller';
 import { FileUploaderService } from './service';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             cache: true,
             load: [mongoConfig, fileVaultConfig],
+            isGlobal: true,
+        }),
+        ServeStaticModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => {
+                const rootPath = configService.get('fileVault.uploadDir');
+                const serveRoot = configService.get('fileVault.serveRoot');
+
+                return [{ rootPath, serveRoot }];
+            },
         }),
     ],
     controllers: [FileUploaderController],
