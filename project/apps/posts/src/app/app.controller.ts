7e5ce08f2fd10b1d25b0e8cfcca7e1240fa0f1e9
Alexander Vinokurov <vinokurov.ag@mail.ru>
2025-02-28 11:40:45 +0300

    edit posts response

--- project/apps/posts/src/app/app.controller.ts
+++ project/apps/posts/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
 import { PostService } from './app.service';
 import { fillDto } from '@project/utils';
 import { PostRdo } from './rdo';
@@ -12,13 +12,16 @@ export class PostController {
         const postEntities = await this.postService.getPosts();
         const posts = postEntities.map((entity) => entity.toPOJO());
 
-        return fillDto(PostRdo, posts);
+        return { statusCode: HttpStatus.OK, data: fillDto(PostRdo, posts) };
     }
 
     @Get('/:id')
     public async show(@Param('id') id: string) {
         const postEntity = await this.postService.getPostById(id);
 
-        return { statusCode: 200, data: fillDto(PostRdo, postEntity.toPOJO()) };
+        return {
+            statusCode: HttpStatus.OK,
+            data: fillDto(PostRdo, postEntity.toPOJO()),
+        };
     }
 }
